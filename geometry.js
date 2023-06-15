e1d34ac0ca59aa47b12400731fd13a75b036c9c8
Oxyn <52625331+Oxynt@users.noreply.github.com>
2023-06-15 19:14:08 +0200

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
-import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
-import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler.js';
+import { GUI } from './lil-gui.module.min.js';
+import { OrbitControls } from './OrbitControls.js';
+import { GLTFLoader } from './GLTFLoader.js';
+import { MeshoptDecoder } from './meshopt_decoder.module.js';
+import { MeshSurfaceSampler } from './MeshSurfaceSampler.js';
 import {
 	Brush,
 	Evaluator,
